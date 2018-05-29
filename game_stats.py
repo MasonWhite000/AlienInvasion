ee70beca10094bbc1312795eb2c3d249d0f1de7f
mcwhit02 <mason.white000@gmail.com>
2018-05-29 08:42:58 -0400

    s

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,7 @@ class GameStats():
         """Initialize stats"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.game_active = True
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
